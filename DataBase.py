4c1ab208b47a74e3e6a4401b5c6881b715ea88e3
Barbuta Cristian <barbuta.cristian1618@gmail.com>
2021-08-23 01:31:51 +0300

    communication with database

--- DataBase.py
+++ DataBase.py
@@ -20,31 +20,57 @@ class DataBase():
         if oldPrice != product.getPrice():
 
             insertCommand = "INSERT INTO products VALUES (NULL, ?, ?, ?, ?)"
-            self.cursor.execute(insertCommand, (product.getName(), product.getPrice(), product.getTime().strftime("%H:%M:%S"), product.getLink()))
-            self.connection.commit()
+            self.cursor.execute(insertCommand, (product.getName(), product.getPrice(), product.getTime(), product.getLink()))
+
+            #i'm not sure if the program should take data from previous runs
+            #self.connection.commit()
     
     def selectAll(self):
         selectAllCommand = "SELECT * FROM products"
         self.cursor.execute(selectAllCommand)
         return self.cursor.fetchall()
-    
-    def getProductByLink(self, link):
-        searchCommand = "SELECT * FROM products WHERE link = ?"
-        self.cursor.execute(searchCommand, (link,))
+
+    def getProductById(self, id):
+        searchCommand = "SELECT * FROM products WHERE id = ?"
+        self.cursor.execute(searchCommand, (id,))
         data = self.cursor.fetchall()[0]
 
         return Products.Product(data[1], data[2], data[3], data[4])
 
     def getPriceByLink(self, link):
-        searchCommand = "SELECT price FROM products WHERE link = ?"
-        self.cursor.execute(searchCommand, (link,))
-        price = self.cursor.fetchall()[-1]
+
+        try:
+            searchCommand = "SELECT price FROM products WHERE link = ?"
+            self.cursor.execute(searchCommand, (link,))
+            price = self.cursor.fetchall()[-1]
+        except:
+            price = -1
 
         return price
     
-    def getMinPrice():
-        pass
+    def getMinPrice(self):
+        products = self.selectAll()
 
-    def getMaxPrice():
-        pass
+        minPrice = float('inf')
+        minPriceId = -1
+        for item in products:
+            if item[2] <= minPrice:
+                minPrice = item[2]
+                minPriceId = item[0]
+        
+        return self.getProductById(minPriceId)
+
+
+    def getMaxPrice(self):
+        products = self.selectAll()
+
+        maxPrice = float('-inf')
+        maxPriceId = -1
+        for item in products:
+            if item[2] >= maxPrice:
+                maxPrice = item[2]
+                maxPriceId = item[0]
+        
+        return self.getProductById(maxPriceId)
+
         
